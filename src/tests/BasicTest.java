f777b283242565f52ab1b8ab8ad4c6767639ba55
Ivan Matejev <ivanmatejev@gmail.com>
2022-10-09 12:54:08 +0200

    Added BasicTest

---
+++ src/tests/BasicTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+public class BasicTest {
+	 
+	protected WebDriver driver;
+ 	protected WebDriverWait wait;
+ 	protected SoftAssert softAssert;
+ 	protected Actions actions;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+    
+	
+	
+	@BeforeClass
+	public void beforeClass() {
+	    System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	    softAssert = new SoftAssert();
+	    actions = new Actions(driver);
+	}
+	 
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get(baseUrl);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
